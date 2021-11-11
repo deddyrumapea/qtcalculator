d72ceb83d6d0bb1b9ba98ef592212211b21c9c61
Deddy Romnan <romnandeddy@gmail.com>
2021-11-11 10:23:03 +0700

    fix: solution units

--- models/QueueModel.js
+++ models/QueueModel.js
@@ -1,3 +1,5 @@
+import TimeUnitHelper from "../helpers/TimeUnitHelper.js";
+
 export default class QueueModel {
   static STORAGE_KEY = "queue_storage";
   static STORAGE_CAPACITY = 4;
@@ -18,19 +20,19 @@ export default class QueueModel {
     return `<p>Service utilization rate 
     \\[U = {\\lambda \\over \\mu}\\]
     \\[U = {${this.lambda} \\over ${this.mu}}\\]
-    \\[U = {${(this.lambda / this.mu).toFixed(2)}}\\]
-    \\[U = {${(this.lambda / this.mu).toFixed(2) * 100}}\\%\\]
+    \\[U = {${this.utilizationRate.toFixed(3)}}\\]
+    \\[U = {${(this.utilizationRate.toFixed(3) * 100).toFixed(1)}}\\%\\]
     </p>
     `;
   }
 
   writeProbabilityOf(n) {
-    let util = (this.lambda / this.mu).toFixed(2);
+    let util = (this.lambda / this.mu).toFixed(3);
     let probability = `<p>Probability of zero customers
     \\[P_n = (1 - U) \\times U^n\\]
     \\[P_${n} = (1 - ${util}) \\times ${util}^${n}\\]
-    \\[P_${n} = (${(1 - util).toFixed(2)}) \\times ${Math.pow(util, n)}\\]
-    \\[P_${n} = ${((1 - util) * Math.pow(util, n)).toFixed(2)}\\]
+    \\[P_${n} = (${(1 - util).toFixed(3)}) \\times ${Math.pow(util, n)}\\]
+    \\[P_${n} = ${((1 - util) * Math.pow(util, n)).toFixed(3)}\\]
     </p>`;
     return probability;
   }
@@ -44,11 +46,8 @@ export default class QueueModel {
     \\[L_Q = {${Math.pow(lambda, 2)} \\over ${mu}(${mu - lambda})}\\]
     \\[L_Q = {${Math.pow(lambda, 2)} \\over ${mu * (mu - lambda)}}\\]
     \\[L_Q = {${Math.pow(lambda, 2)} \\over ${mu * (mu - lambda)}}\\]
-    \\[L_Q = {${(Math.pow(lambda, 2) / (mu * (mu - lambda))).toFixed(2)}}\\]
-    \\[L_Q \\approx {${(
-      Math.pow(lambda, 2) /
-      (mu * (mu - lambda))
-    ).toFixed()}\\;units}\\] 
+    \\[L_Q = {${this.lengthOfQueue.toFixed(3)}}\\]
+    \\[L_Q \\approx {${this.lengthOfQueue.toFixed()}\\;units}\\] 
     </p>`;
   }
 
@@ -57,7 +56,11 @@ export default class QueueModel {
     \\[W = {1 \\over \\mu - \\lambda}\\]
     \\[W = {1 \\over ${this.mu}- ${this.lambda}}\\]
     \\[W = {1 \\over ${this.mu - this.lambda}}\\]
-    \\[W = {${(1 / (this.mu - this.lambda)).toFixed(2)}\\;${this.timeUnit}}\\]
+    \\[W = {${this.waitingTime.toFixed(3)}\\;${this.timeUnit}}\\]
+    \\[W = {${TimeUnitHelper.formatToHms(
+      this.timeUnit,
+      this.waitingTime.toFixed(3)
+    )}}\\]
     </p>`;
   }
 
@@ -68,9 +71,11 @@ export default class QueueModel {
     \\[W_Q = {${this.lambda} \\over ${this.mu}(${this.mu - this.lambda})}\\]
     \\[W_Q = {${this.lambda} \\over ${this.mu * (this.mu - this.lambda)}}\\]
     \\[W_Q = {${this.lambda} \\over ${this.mu * (this.mu - this.lambda)}}\\]
-    \\[W_Q = {${(this.lambda / (this.mu * (this.mu - this.lambda))).toFixed(
-      2
-    )}\\;${this.timeUnit}}\\]
+    \\[W_Q = {${this.waitingTimeInQueue.toFixed(3)}\\;${this.timeUnit}}\\]
+    \\[W = {${TimeUnitHelper.formatToHms(
+      this.timeUnit,
+      this.waitingTimeInQueue.toFixed(3)
+    )}}\\]
     </p>`;
   }
 
@@ -78,7 +83,11 @@ export default class QueueModel {
     return `<p>Average waiting time in service
     \\[W_S = {1 \\over \\mu}\\]
     \\[W_S = {1 \\over ${this.mu}}\\]
-    \\[W_S = ${(1 / this.mu).toFixed(2)}\\;${this.timeUnit}\\]
+    \\[W_S = ${this.waitingTimeInService.toFixed(3)}\\;${this.timeUnit}\\]
+    \\[W = {${TimeUnitHelper.formatToHms(
+      this.timeUnit,
+      this.waitingTimeInService.toFixed(3)
+    )}}\\]
     </p>`;
   }
 }
